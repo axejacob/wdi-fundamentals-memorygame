ff94251525ae27e3e2b2cb99d82c170438d75da9
Axel <email@axeljacob.com>
2019-06-24 01:15:23 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,32 @@
-console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+},
+{
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+},
+{
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.pn"
+},
+{
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-//Idiot,[1]-> var cardInPlay[here is cardOne, now is cardTwo]
-
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId].rank);
 if (cardsInPlay.length === 2) {
 console.log("There are 2 cards");
 }
@@ -22,3 +36,16 @@ alert("You found a match!");
 } else {
     alert("Sorry try again.");
 }
+};
+
+cardsInPlay.push(cards[cardId]);
+
+function flipCard(){
+    console.log(0);
+};
+
+function flipCard(){
+    console.log(2);
+};
+
+console.log("cardImage");
